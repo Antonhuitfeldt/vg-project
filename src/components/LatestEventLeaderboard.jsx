73feab3bfdd18eb050c983018e5ed8f7828084f8
Antonhuitfeldt <anton.huitfeldt@hotmail.com>
2026-06-04 21:07:03 +0200

    integrated data from the api to the chart

--- src/components/LatestEventLeaderboard.jsx
+++ src/components/LatestEventLeaderboard.jsx
@@ -9,17 +9,6 @@ import {
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 
-const samplePlayers = [
-    { name: 'Scottie Scheffler', score: 18 },
-    { name: 'Rory McIlroy', score: 15 },
-    { name: 'Xander Schauffele', score: 12 },
-    { name: 'Collin Morikawa', score: 10 },
-    { name: 'Viktor Hovland', score: 8 },
-    { name: 'Tommy Fleetwood', score: 6 },
-    { name: 'Jon Rahm', score: 4 },
-    { name: 'Ludvig Åberg', score: 2 },
-  ]
-
 ChartJS.register(
     CategoryScale,
     LinearScale,
@@ -40,22 +29,30 @@ ChartJS.register(
         text: 'Tävlingens namn',
       },
     },
-  };
-  
-  const labels = samplePlayers.map(p => p.name)
-  
-  const data = {
-    labels,
-    datasets: [
-      {
-        label: 'Score under par',
-        data: samplePlayers.map(p => p.score),
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      },
-    ],
+    scales: { 
+      y: {
+        reverse: true
+      }
+    }
   };
   
   const LatestEventLeaderboard = ({ leaderboard }) => {
+    if (!leaderboard) return <p>Loading...</p>
+    
+    const top10 = leaderboard.leaderboardRows.slice(0, 10)
+    const labels = top10.map(p => [p.firstName + ' ' + p.lastName, p.position])
+
+    const data = {
+      labels,
+      datasets: [
+        {
+          label: 'Score under par',
+          data: top10.map(p => p.total),
+          backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        },
+      ],
+    };
+
     return(
         <div id="leaderboard" className='w-5/6 mx-auto p-10'>
             <Bar options={options} data={data} />
